1c180fabfd04583c728bf680e145f8ab1f6a4105
kk <kk@unit9.com>
2020-01-23 02:34:33 +0100

    few tweaks

--- engine3d.py
+++ engine3d.py
@@ -13,6 +13,7 @@ cv = int(screen_size[0]/2), int(screen_size[1]/2)
 
 
 class Vec3d(object):
+    __slots__ = ['x', 'y', 'z']
 
     def __init__(self, x, y, z):
         self.x = x
@@ -91,23 +92,23 @@ class Object3d(object):
 class Camera3d(object):
     
 
-    def __init__(self, pos :Vec3d=Vec3d(0,0,-5), angle=(0, math.pi/2), fov=1200):
+    def __init__(self, pos :Vec3d=Vec3d(0,0,-5), look_at=(0, 0, 0), fov=1200):
         
         self.pos = copy.copy(pos)
         self.fov = fov
         self.show_verts = True
         self.show_edges = True
         self.show_faces = False #TBD
-        self.angle = angle # to be implemented ;)
+        self.look_at = look_at # to be implemented ;)
 
     def project(self, o: Object3d, vert: Vec3d):
         """
         verts just a triple i coultn't find more elegant way atm :(
         """                 
         _x, _y, _z = o.pos        
-        x,y,z = -_x+vert.x-self.pos.x, -_y+vert.y-self.pos.y, -_z+vert.z+self.pos.z
-        x = x*self.fov/z+cv[0]
-        y = y*self.fov/z+cv[1]
+        x, y, z = -_x + vert.x - self.pos.x, -_y + vert.y - self.pos.y, -_z + vert.z + self.pos.z
+        x = x * self.fov / z + cv[0]
+        y = y * self.fov / z + cv[1]
         return Vec3d(x,y,z)
 
     def draw(self, o: Object3d):   
@@ -133,9 +134,9 @@ screen = pygame.display.set_mode(screen_size)
 screen.fill(bgc)
 #TODO class
 
+#SETUP
+
 player_movement = 0.7
-
-
 world = list() 
 # ^ let's say it's our world if not the challange and limited time 
 # it could be nicely 'classified'
@@ -172,53 +173,23 @@ depth = 3
 for idx_z, row in enumerate(_map):
     
     for idx_x, cell in enumerate(list(row)):
-        for z in range(depth):
-        
+        for z in range(depth):        
             if cell == '1':
                 world2.append(Object3d(Vec3d(idx_x,idx_z,z), color=(255,0, int(z*255/depth))))
             
 cam2 = Camera3d(Vec3d(-5,-5,-20))
-# print(world2)
 
-
-
-# box2 = Object3d(Vec3d(-1,3,2), size=Vec3d(0.1,0.2,0.1), color=(0,255,0))
-# cam_user = Camera3d() #user can move it around
-# cam_retarded = Camera3d() 
-
-
+#UPDATE
 dt = 0
 while(True):
-    dt += 1
-    # print(cam_user.pos.z, cam_retarded.pos.z)
-
-    # cam_user.pos.z -= dt*0.003
+    dt += 1 #todo real time pass
     time.sleep(0.02)
     screen.fill(bgc)
 
-    #steady cam
-    # box2.rotate((0.05,0,0))
-    # cam_user.draw(box2)
-
-    #dungeon
-    x = 0
-    for o in world2:
-        x += 1
+    for _x, o in enumerate(world2):
+        x = _x % 10
         o.rotate(Vec3d(math.sin(dt/20+x/5)*0.05,0.01,0))        
-        cam2.draw(o)
-        if x == 10: x = 0
-
-    ###retarded one
-    #playing directly with objects
-    # x ,y = math.sin(dt/7)*2, math.cos(dt/7)*2
-    # world[3].pos=(x,y,3)
-    # world[5].rotate((0,0,0.2))
-    #playing with camera
-    # cam_retarded.pos = Vec3d(math.sin(dt/25)*2, math.cos(dt/25)*2,(math.cos(dt/21)-3)*2)
-    # world[0].rotate(Vec3d(0, 0, 0.09*math.sin(dt*0.1)))
-    # for o in world:
-    #     o.rotate((0, 0.03, 0))
-    #     cam_retarded.draw(o)
+        cam2.draw(o)        
 
     pygame.display.flip()
         
